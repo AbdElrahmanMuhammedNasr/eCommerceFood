a2bae743421f24648016beabe9e096910b695e42
AbdElrahmanMuhammedNasr <abdo300nasr@gmail.com>
2020-08-28 01:35:45 +0200

    init

--- src/app/home/snapshot-porducts/snapshot-porducts.component.ts
+++ src/app/home/snapshot-porducts/snapshot-porducts.component.ts
@@ -10,9 +10,83 @@ export class SnapshotPorductsComponent implements OnInit {
 
   constructor(private router:Router) { }
 
-  snap =[]
+  productShapshots =[]
   ngOnInit(): void {
-    this.snap=[1,1,1,1,1,1,1,1,1,1,1,1]
+    this.productShapshots=[
+    {
+      id:1,
+      image:'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132__340.jpg',
+      name:'Stack',
+      price:50,
+      discount:false,
+      categoryType:'meat'
+
+    },
+    {
+      id:2,
+      image:'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132__340.jpg',
+      name:'Stack 2',
+      price:50,
+      discount:10,
+      categoryType:'meat'
+
+    }, {
+      id:3,
+      image:'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132__340.jpg',
+      name:'Stack',
+      price:50,
+      discount:false,
+      categoryType:'meat'
+
+    },
+    {
+      id:4,
+      image:'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132__340.jpg',
+      name:'Stack 2',
+      price:50,
+      discount:10,
+      categoryType:'meat'
+
+    }, {
+      id:5,
+      image:'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132__340.jpg',
+      name:'Stack',
+      price:50,
+      discount:false,
+      categoryType:'meat'
+
+    },
+    {
+      id:6,
+      image:'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132__340.jpg',
+      name:'Stack 2',
+      price:50,
+      discount:10,
+      categoryType:'meat'
+
+    }, {
+      id:5,
+      image:'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132__340.jpg',
+      name:'Stack',
+      price:50,
+      discount:false,
+      categoryType:'meat'
+
+    },
+    {
+      id:6,
+      image:'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132__340.jpg',
+      name:'Stack 2',
+      price:50,
+      discount:10,
+      categoryType:'meat'
+
+    },
+
+
+
+
+  ]
   }
 
   onGoToDetails(){
